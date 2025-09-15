b03e6c5325d0476c05a76b022d3eab01f272ce80
Wartent15 <163977218+Wartent15@users.noreply.github.com>
2025-09-15 16:50:13 +0300

    Update Review.cs

--- 15.9/Review.cs
+++ 15.9/Review.cs
@@ -5,14 +5,12 @@ public class Review
     [Key]
     public int ID { get; set; }
 
-    // Внешние ключи: курс и студент
     public int CourseID { get; set; }
     public Course Course { get; set; }
 
     public int StudentID { get; set; }
     public Student Student { get; set; }
 
-    // Рейтинг от 0 до 10
     [Range(0, 10)]
     public int Rating { get; set; }
 }
