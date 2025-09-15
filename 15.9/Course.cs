a13323df9ec11eb21d9d0e80b3834449a364e6af
Wartent15 <163977218+Wartent15@users.noreply.github.com>
2025-09-15 16:49:04 +0300

    Course.cs

--- 15.9/Course.cs
+++ 15.9/Course.cs
@@ -12,15 +12,11 @@ public class Course
 
     public string Description { get; set; }
 
-    // Длительность в часах
     public int DurationHours { get; set; }
 
-    // Цена в валюте вашей БД (например, USD)
     [Column(TypeName = "decimal(18,2)")]
     public decimal Price { get; set; }
 
-    // Навигация: один ко многим к преподавателям (множество преподавателей могут привязаться к курсу)
     public ICollection<Instructor> Instructors { get; set; }
 
-    // Навигация: отзывы по курсу
     public ICollection<Review> Reviews { get; set; }
